a95a743eab69b802d3c8fc042947a5dc608cbd37
root <root@Debian-60-squeeze-64-minimal.(none)>
2012-02-07 01:40:41 +0100

    changes to default installation values

--- restart.sh
+++ restart.sh
@@ -5,7 +5,7 @@ export RUBY_HEAP_SLOTS_INCREMENT=2000
 export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=600000
 export RUBY_HEAP_FREE_MIN=65536
-cd /home/sites/diaspora
+cd /home/happeningineu
 /etc/init.d/diaspora stop
 killall -9 ruby1.9.1
 killall -9 ruby
