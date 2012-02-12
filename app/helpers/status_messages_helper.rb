da7466afeae157dc16b6d8b7394d66ea6b07e162
Rove Monteux <rove.monteux@fluid-rock.com>
2012-02-12 02:36:52 +0100

    added mapping support

--- app/helpers/status_messages_helper.rb
+++ app/helpers/status_messages_helper.rb
@@ -36,9 +36,10 @@ def googlemaps(str)
     while vurl= str.match(/^map:.*$/ix) do
       vurl = vurl.to_s()
       unless vurl.nil?
+        vurl[0..3] = ''
         originalurl = vurl.strip
         embedurl = URI.escape(originalurl)
-        embed = "<br/><img src=\"https://maps.googleapis.com/maps/api/staticmap?center="+embedurl+"&zoom=15&size=560x315&maptype=hybrid&sensor=false\"/><br/><br/><img src=\"https://maps.googleapis.com/maps/api/staticmap?center="+embedurl+"&zoom=17&size=560x315&maptype=hybrid&sensor=false\"/><br/><div style=\"width:560px; margin-top: -5px; margin-bottom: 15px;\"><span style=\"font-size: 9px; float: right; position: relative; margin-top: -5px; margin-bottom: -40px; \"><a href=\"http://maps.google.com/?q="+embedurl+"\" target=\"_blank\" rel=\"nofollow\">"+originalurl.strip.titleize+"</a></span></div>"
+        embed = "<br/><img src=\"https://maps.googleapis.com/maps/api/staticmap?center="+embedurl+"&zoom=15&size=560x315&maptype=hybrid&sensor=false\"/><br/><br/><img src=\"https://maps.googleapis.com/maps/api/staticmap?center="+embedurl+"&zoom=17&size=560x315&maptype=hybrid&sensor=false\"/><br/><div style=\"width:560px; margin-top: -5px; margin-bottom: 20px;\"><span style=\"font-size: 9px; float: right; position: relative; margin-top: -5px; margin-bottom: -40px; \"><a href=\"http://maps.google.com/?q="+embedurl+"\" target=\"_blank\" rel=\"nofollow\">"+originalurl.strip.titleize+"</a></span></div>"
         begin
           str[originalurl] = embed
         rescue Exception=>e
