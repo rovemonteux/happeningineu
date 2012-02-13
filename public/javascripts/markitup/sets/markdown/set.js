74cda73f7cb48f0bf1a01762c1bd9854b0b4fb65
Rove Monteux <rove.monteux@fluid-rock.com>
2012-02-13 12:23:23 +0100

    miscellaneous fixes and new editor

---
+++ public/javascripts/markitup/sets/markdown/set.js
@@ -0,0 +1,55 @@
+// -------------------------------------------------------------------
+// markItUp!
+// -------------------------------------------------------------------
+// Copyright (C) 2008 Jay Salvat
+// http://markitup.jaysalvat.com/
+// -------------------------------------------------------------------
+// MarkDown tags example
+// http://en.wikipedia.org/wiki/Markdown
+// http://daringfireball.net/projects/markdown/
+// -------------------------------------------------------------------
+// Feel free to add more tags
+// -------------------------------------------------------------------
+mySettings = {
+	previewParserPath:	'',
+	onShiftEnter:		{keepDefault:false, openWith:'\n\n'},
+	markupSet: [
+		{name:'First Level Heading', key:'1', openWith:'# ', placeHolder:'H1...'},
+		{name:'Second Level Heading', key:'2', openWith:'## ', placeHolder:'H2...'},
+		{name:'Heading 3', key:'3', openWith:'### ', placeHolder:'H3...' },
+		{name:'Heading 4', key:'4', openWith:'#### ', placeHolder:'H4...' },
+		{name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'H5...' },
+		{name:'Heading 6', key:'6', openWith:'###### ', placeHolder:'H6...' },
+		{separator:'---------------' },		
+		{name:'Bold', key:'B', openWith:'**', closeWith:'**'},
+		{name:'Italic', key:'I', openWith:'_', closeWith:'_'},
+		{separator:'---------------' },
+		{name:'Bulleted List', openWith:'- ' },
+		{name:'Numeric List', openWith:function(markItUp) {
+			return markItUp.line+'. ';
+		}},
+        {separator:'---------------' },
+        {name:'Tag', key:'T', openWith:'#', closeWith:'[![Tag:!:happeningin]!]'},
+        {separator:'---------------' },
+		{name:'YouTube', key:'Y', openWith:'[![YouTube URL:!:https://www.youtube.com/]!]'},
+        {name:'Dailymotion', key:'D', openWith:'[![Dailymotion URL:!:https://www.dailymotion.com/]!]'},
+        {name:'Vimeo', key:'V', openWith:'[![Vimeo URL:!:https://www.vimeo.com/]!]'},
+        {separator:'---------------' },
+        {name:'Podcast', key:'P', openWith:'[![Podcast .mp3 URL:!:https://site.com/podcast.mp3]!]'},
+        {name:'Flash', key:'F', openWith:'[![Flash .swf URL:!:https://site.com/flash.swf]!]'},
+        {separator:'---------------' },
+        {name:'Map', key:'M', closeWith:'[![Map:!:Mikulov, Czech Republic]!]', openWith:'map: '}
+	]
+}
+
+// mIu nameSpace to avoid conflict.
+miu = {
+	markdownTitle: function(markItUp, char) {
+		heading = '';
+		n = $.trim(markItUp.selection||markItUp.placeHolder).length;
+		for(i = 0; i < n; i++) {
+			heading += char;
+		}
+		return '\n'+heading;
+	}
+}
